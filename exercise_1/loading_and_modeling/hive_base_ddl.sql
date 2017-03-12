da72506ad671c570a60529311a7f8b38606fccc0
root <root@ip-10-51-199-84.ec2.internal>
2017-03-12 23:52:48 +0000

    Added ddl, fixed load_data_lake

---
+++ exercise_1/loading_and_modeling/hive_base_ddl.sql
@@ -0,0 +1,147 @@
+DROP TABLE IF EXISTS hospitals;
+CREATE EXTERNAL TABLE hospitals (
+	provider_id STRING, 
+	hospital_name STRING, 
+	address STRING, 
+	city STRING, 
+	state STRING, 
+	zip_code INT, 
+	county_name STRING, 
+	phone_number INT,
+	hospital_type STRING, 
+	hospital_ownership STRING, 
+	emergency_services STRING
+	)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+	"separatorChar" = ",",
+	"quoteChar" = '"',
+	"escapeChar" = '\\'
+	)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/hospitals';
+
+
+DROP TABLE IF EXISTS readmissions;
+CREATE EXTERNAL TABLE readmissions (
+	provider_id STRING, 
+	hospital_name STRING,
+	address STRING, 
+	city STRING, 
+	state STRING, 
+	zip_code INT, 
+	county_name STRING, 
+	phone_number INT,
+	measure_name STRING,
+	measure_id STRING, 
+	compared_to_national STRING, 
+	denominator STRING, 
+	score STRING, 
+	lower_estimate STRING, 
+	higher_estimate STRING,
+	footnote STRING,
+	measure_start_date STRING,
+	measure_end_date STRING
+	)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+	"separatorChar" = ",",
+	"quoteChar" = '"',
+	"escapeChar" = '\\'
+	)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/readmissions';
+
+
+DROP TABLE IF EXISTS effective_care;
+CREATE EXTERNAL TABLE effective_care (
+	provider_id STRING, 
+	hospital_name STRING,
+	address STRING, 
+	city STRING, 
+	state STRING, 
+	zip_code INT, 
+	county_name STRING, 
+	phone_number INT,
+	condition STRING, 
+	measure_id STRING, 
+	measure_name STRING,
+	score STRING, 
+	sample STRING, 
+	footnote STRING,
+	measure_start_date STRING,
+	measure_end_date STRING
+	)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+	"separatorChar" = ",",
+	"quoteChar" = '"',
+	"escapeChar" = '\\'
+	)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/effective_care';
+
+
+DROP TABLE IF EXISTS measures;
+CREATE EXTERNAL TABLE measures (
+	measure_name STRING, 
+	measure_id STRING, 
+	measure_start_quarter STRING, 
+	measure_start_date STRING,
+	measure_end_quarter STRING, 
+	measure_end_date STRING
+	)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+	"separatorChar" = ",",
+	"quoteChar" = '"',
+	"escapeChar" = '\\'
+	)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/measures';
+
+
+DROP TABLE IF EXISTS survey_responses;
+CREATE EXTERNAL TABLE survey_responses (
+	provider_id STRING, 
+	hospital_name STRING,
+	address STRING, 
+	city STRING, 
+	state STRING, 
+	zip_code INT, 
+	county_name STRING, 
+	communication_with_nurses_achievement_points STRING, 
+	communication_with_nurses_improvement_points STRING, 
+	communication_with_nurses_dimension_score STRING, 
+	communication_with_doctors_achievement_points STRING, 
+	communication_with_doctors_improvement_points STRING, 
+	communication_with_doctors_dimension_score STRING, 
+	responsiveness_of_hospital_staff_achievement_points STRING, 
+	responsiveness_of_hospital_staff_improvement_points STRING, 
+	responsiveness_of_hospital_staff_dimension_score STRING, 
+	pain_management_achievement_points STRING, 
+	pain_management_improvement_points STRING, 
+	pain_management_dimension_score STRING, 
+	communication_about_medicines_achievement_points STRING, 
+	communication_about_medicines_improvement_points STRING, 
+	communication_about_medicines_dimension_score STRING, 
+	cleanliness_and_quietness_of_hospital_environment_achievement_points STRING, 
+	cleanliness_and_quietness_of_hospital_environment_improvement_points STRING, 
+	cleanliness_and_quietness_of_hospital_environment_dimension_score STRING, 
+	discharge_information_achievement_points STRING, 
+	discharge_information_improvement_points STRING, 
+	discharge_information_dimension_score STRING, 
+	overall_rating_of_hospital_achievement_points STRING, 
+	overall_rating_of_hospital_improvement_points STRING, 
+	overall_rating_of_hospital_dimension_score STRING, 
+	hcahps_base_score STRING, 
+	hcahps_consistency_score STRING
+	)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+	"separatorChar" = ",",
+	"quoteChar" = '"',
+	"escapeChar" = '\\'
+	)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/surveys_responses';
--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -1,15 +1,26 @@
-tail -n +2 Hospital\ General\ Information.csv > hospitals.csv
-tail -n +2 Timely\ and\ Effective\ Care\ -\ Hospital.csv > effective_care.csv
-tail -n +2 Readmissions\ and\ Deaths\ -\ Hospital.csv > readmissions.csv
-tail -n +2 Measure\ Dates.csv > measures.csv
-tail -n +2 hvbp_hcahps_05_28_2015.csv > surveys_responses.csv
+data_dir=$1
+
+tail -n +2 $data_dir/Hospital\ General\ Information.csv > hospitals.csv
+tail -n +2 $data_dir/Timely\ and\ Effective\ Care\ -\ Hospital.csv > effective_care.csv
+tail -n +2 $data_dir/Readmissions\ and\ Deaths\ -\ Hospital.csv > readmissions.csv
+tail -n +2 $data_dir/Measure\ Dates.csv > measures.csv
+tail -n +2 $data_dir/hvbp_hcahps_05_28_2015.csv > surveys_responses.csv
 
 # move these files into a common directory
-mkdir hospital_data
-mv -t hospital_data hospitals.csv effective_care.csv readmissions.csv measures.csv survey_responses.csv
+mkdir $data_dir/hospital_data_no_headers
+mv -t $data_dir/hospital_data_no_headers $data_dir/hospitals.csv $data_dir/effective_care.csv $data_dir/readmissions.csv $data_dir/measures.csv $data_dir/survey_responses.csv
 
 # move the directory to hdfs
 su - hdfs
-hdfs dfs -mkdir /user/w205/hospital_compare
-hdfs dfs -put /hospital_data/* /user/w205/hospital_compare
+cd $data_dir/hospital_data_no_headers
+hdfs dfs -mkdir -p /user/w205/hospital_compare/hospitals
+hdfs dfs -mkdir -p /user/w205/hospital_compare/effective_care
+hdfs dfs -mkdir -p /user/w205/hospital_compare/readmissions
+hdfs dfs -mkdir -p /user/w205/hospital_compare/measures
+hdfs dfs -mkdir -p /user/w205/hospital_compare/surveys_responses
+hdfs dfs -put hospitals.csv /user/w205/hospital_compare/hospitals
+hdfs dfs -put effective_care.csv /user/w205/hospital_compare/effective_care
+hdfs dfs -put readmissions.csv /user/w205/hospital_compare/readmissions
+hdfs dfs -put measures.csv /user/w205/hospital_compare/measures
+hdfs dfs -put surveys_responses.csv /user/w205/hospital_compare/surveys_responses
 
